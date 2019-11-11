4dfc431e4f15d8ab9e5e1fd3ad2e9af12e6da962
Ryan Hartman <rhartman1239@gmail.com>
2019-11-10 19:03:27 -0500

    Mobile navbare

---
+++ src/scripts/app.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+  const accordionButton = document.querySelector("#menu_btn");
+
+  accordionButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    const panel = this.nextElementSibling;
+    if (panel.classList.contains("opened")) {
+      panel.classList.remove("opened");
+      accordionButton.classList.remove("active");
+    } else {
+      panel.classList.add("opened");
+      accordionButton.classList.add("active");
+    }
+  });
+};
