31d593d67eebfd1c8332055b59f183e21e44ee69
CelinaPeralta <celper19@bergen.org>
2019-12-05 12:30:06 -0500

    pls

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -2,6 +2,9 @@
 function update(){
     //this should reset on the signup page but we'll leave it for now.
     if (sessionStorage.name == undefined) {
+        document.getElementById("current-user-name").addEventListener("mousedown", function(){
+            window.location.href = "#register";
+        });
         return;
     }
     if (sessionStorage.isTutor == "false") {
